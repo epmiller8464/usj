07d2dba7b9d8e631fdb5f6093e785350ce124011
Eric Miller <epmiller8464@gmail.com>
2015-12-09 16:42:37 -0600

    initial commit

---
+++ MainViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  usj
+//
+//  Created by ghostmac on 12/8/15.
+//  Copyright © 2015 ghostmac. All rights reserved.
+//
+
+import UIKit
+import MaterialKit
+
+class MainViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+		// Toggle SideNavigationViewController.
+		let img: UIImage? = UIImage(named: "ic_create_white")
+		let fabButton: FabButton = FabButton()
+		fabButton.setImage(img, forState: .Normal)
+		fabButton.setImage(img, forState: .Highlighted)
+		fabButton.addTarget(self, action: "handleFabButton", forControlEvents: .TouchUpInside)
+		
+		view.addSubview(fabButton)
+		fabButton.translatesAutoresizingMaskIntoConstraints = false
+		MaterialLayout.alignFromBottomRight(view, child: fabButton, bottom: 16, right: 16)
+		MaterialLayout.size(view, child: fabButton, width: 64, height: 64)
+
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+	
+	func handleFabButton() {
+		sideNavigationViewController?.toggle()
+	}
+
+
+}
+
---
+++ SideNavViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SideNavViewController.swift
+//  usj
+//
+//  Created by ghostmac on 12/8/15.
+//  Copyright © 2015 ghostmac. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SideNavViewController: UIViewController {
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+}
