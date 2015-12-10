02d8e88a2fb1ee62135267092567e9afcf372c8e
Eric Miller <epmiller8464@gmail.com>
2015-12-09 18:59:02 -0600

    views

--- SideNavViewController.swift
+++ SideNavViewController.swift
@@ -9,7 +9,7 @@
 import Foundation
 import UIKit
 
-class SideNavViewController: UIViewController {
+class SideNavViewController: UITableViewController {
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
