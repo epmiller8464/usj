f39ebd210157fa96aaa83960b876ee1ffe67cf2d
Eric Miller <epmiller8464@gmail.com>
2015-12-10 23:12:02 -0600

    ...

--- DetailViewController.swift
+++ DetailViewController.swift
@@ -20,13 +20,13 @@ import UIKit
 //	}
 //}
 
-class DetailViewController: UIViewController ,UIPickerViewDelegate,UIPickerViewDataSource{
+class DetailViewController: UIViewController {//,UIPickerViewDelegate,UIPickerViewDataSource{
 	
 	@IBOutlet weak var fnTextField :UITextField?
 	@IBOutlet weak var lnTextField :UITextField?
 	@IBOutlet weak var emailTextField :UITextField?
 	@IBOutlet weak var miTextField :UITextField?
-	@IBOutlet weak var agePicker : UIPickerView?
+//	@IBOutlet weak var agePicker : UIPickerView?
 	
 	
 //	init(){
@@ -40,7 +40,7 @@ class DetailViewController: UIViewController ,UIPickerViewDelegate,UIPickerViewD
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		//		fnTextField?.borderStyle
-		let border = CALayer()
+		var border = CALayer()
 		let width = CGFloat(1.0)
 		let color = UIColor(red: 0.33, green: 0.33, blue: 0.33, alpha: 0.2)
 		border.borderColor = color.CGColor
@@ -48,9 +48,17 @@ class DetailViewController: UIViewController ,UIPickerViewDelegate,UIPickerViewD
 		border.borderWidth = width
 		self.fnTextField?.layer.addSublayer(border)
 		self.fnTextField?.layer.masksToBounds = true
+		border = CALayer()
+		border.borderColor = color.CGColor
+		border.frame = CGRect(x: 0, y: (self.emailTextField?.frame.size.height)! - width, width:  (self.emailTextField?.frame.size.width)!, height: (self.emailTextField?.frame.size.height)!)
+		border.borderWidth = width
+		self.emailTextField?.layer.addSublayer(border)
+		self.emailTextField?.layer.masksToBounds = true
+//		self.miTextField?.layer.addSublayer(border)
+//		self.miTextField?.layer.masksToBounds = true
 		
 		//		self.agePicker?.dataSource = AgePickerViewDataSource();
-		self.ageRange = loadAgeRange();
+//		self.ageRange = loadAgeRange();
 		
 	}
 	
@@ -81,18 +89,18 @@ class DetailViewController: UIViewController ,UIPickerViewDelegate,UIPickerViewD
 		return range;
 	}
 	
-	@objc func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
-		return 1;
-	}
-	
-	@objc func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-		
-		return self.ageRange.count;
-	}
-	
-	func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-		return self.ageRange[row]
-	}
+//	@objc func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+//		return 1;
+//	}
+//	
+//	@objc func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+//		
+//		return self.ageRange.count;
+//	}
+//	
+//	func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+//		return self.ageRange[row]
+//	}
 }
 //class DetailViewController: UITableViewController {
 //
