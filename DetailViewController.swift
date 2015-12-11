538c95092c472a063940f353a43f5a007f3f8156
Eric Miller <epmiller8464@gmail.com>
2015-12-10 22:35:23 -0600

    ...

--- DetailViewController.swift
+++ DetailViewController.swift
@@ -2,26 +2,55 @@
 //
 import UIKit
 //import Alamofire
+//
+//@objc class AgePickerViewDataSource : NSObject, UIPickerViewDataSource{
+//	var maxAge : Int = 100
+//	var minAge : Int = 15
+//	override init(){
+//		super.init()
+//	}
+//
+//	 @objc func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+//		return 1;
+//	}
+//
+//	  @objc func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+//
+//		return self.maxAge - self.minAge;
+//	}
+//}
 
-class DetailViewController: UIViewController {
+class DetailViewController: UIViewController ,UIPickerViewDelegate,UIPickerViewDataSource{
 	
-@IBOutlet weak var fnTextField :UITextField?
+	@IBOutlet weak var fnTextField :UITextField?
 	@IBOutlet weak var lnTextField :UITextField?
 	@IBOutlet weak var emailTextField :UITextField?
-		@IBOutlet weak var miTextField :UITextField?
+	@IBOutlet weak var miTextField :UITextField?
+	@IBOutlet weak var agePicker : UIPickerView?
+	
+	
+//	init(){
+//		super.init(;
+//	}
+//
+//	required init?(coder aDecoder: NSCoder) {
+//	    fatalError("init(coder:) has not been implemented")
+//	}
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		//		fnTextField?.borderStyle
 		let border = CALayer()
-		let width = CGFloat(2.0)
-		border.borderColor = UIColor.darkGrayColor().CGColor
+		let width = CGFloat(1.0)
+		let color = UIColor(red: 0.33, green: 0.33, blue: 0.33, alpha: 0.2)
+		border.borderColor = color.CGColor
 		border.frame = CGRect(x: 0, y: (self.fnTextField?.frame.size.height)! - width, width:  (self.fnTextField?.frame.size.width)!, height: (self.fnTextField?.frame.size.height)!)
-//
-		
 		border.borderWidth = width
 		self.fnTextField?.layer.addSublayer(border)
 		self.fnTextField?.layer.masksToBounds = true
+		
+		//		self.agePicker?.dataSource = AgePickerViewDataSource();
+		self.ageRange = loadAgeRange();
 		
 	}
 	
@@ -34,11 +63,35 @@ class DetailViewController: UIViewController {
 	}
 	
 	@IBAction func edit(sender: AnyObject) {
-
+		
 	}
 	
 	@IBAction func save(sender: AnyObject) {
 		print("saving");
+	}
+	var maxAge : Int = 100
+	var minAge : Int = 15
+	var ageRange : [String] = [];
+	
+	func loadAgeRange() -> [String] {
+		var range : [String] = [];
+		for  index in minAge...maxAge{
+		range.insert(index.description, atIndex: index - minAge)
+		}
+		return range;
+	}
+	
+	@objc func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+		return 1;
+	}
+	
+	@objc func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+		
+		return self.ageRange.count;
+	}
+	
+	func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+		return self.ageRange[row]
 	}
 }
 //class DetailViewController: UITableViewController {
