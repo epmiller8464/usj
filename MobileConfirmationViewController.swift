b264f1d2e13cd3a8d47a0aaf39becf1c768dfd42
Eric Miller <epmiller8464@gmail.com>
2015-12-12 07:20:44 -0600

    ..

--- MobileConfirmationViewController.swift
+++ MobileConfirmationViewController.swift
@@ -14,9 +14,7 @@ class MobileConfirmationViewController: UIViewController {
 	@IBOutlet weak var cancelButton : FlatButton!
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		
-		// Examples of using NavigationBarView
-		prepareNavigationBarViewExample()
+		prepareNavigationBarView()
 	}
 	
 	/**
@@ -31,7 +29,7 @@ class MobileConfirmationViewController: UIViewController {
 	:name:	prepareNavigationBarViewExample
 	:description:	General usage example.
 	*/
-	func prepareNavigationBarViewExample() {
+	func prepareNavigationBarView() {
 		
 		// Stylize.
 		navigationBarView.backgroundColor = MaterialColor.indigo.darken1
@@ -41,57 +39,25 @@ class MobileConfirmationViewController: UIViewController {
 		navigationBarView.statusBarStyle = .LightContent
 		
 		// Title label.
-		let titleLabel: UILabel = UILabel()
+		let titleLabel: UILabel = UILabel(frame: CGRectMake(15, 15, 60, 30))
 		titleLabel.text = "UStream Justice"
 		titleLabel.textAlignment = .Center
 		titleLabel.textColor = MaterialColor.white
-		titleLabel.font = RobotoFont.regularWithSize(20)
+		titleLabel.font = RobotoFont.regularWithSize(12)
 		navigationBarView.titleLabel = titleLabel
-		navigationBarView.titleLabelInsetsRef.left = 64
-		
-		// Detail label
-//		let detailLabel: UILabel = UILabel()
-//		detailLabel.text = "UStream Justice"
-//		detailLabel.textAlignment = .Left
-//		detailLabel.textColor = MaterialColor.white
-//		detailLabel.font = RobotoFont.regularWithSize(12)
-//		navigationBarView.detailLabel = detailLabel
-//		navigationBarView.detailLabelInsetsRef.left = 64
-//		
-		// Menu button.
-//		let cancelLabel: UILabel = UILabel()
-//		cancelLabel.text = "Cancel"
-//		cancelLabel.textAlignment = .Left
-//		cancelLabel.textColor = MaterialColor.white
-//		cancelLabel.font = RobotoFont.regularWithSize(12)
-//		let cancelButton: FlatButton = FlatButton(frame: CGRectMake(107, 107, 200, 65))
-//		cancelButton.pulseColor = MaterialColor.white
-//		cancelButton.pulseFill = true
-//		cancelButton.pulseScale = false
-//		cancelButton.setTitle("Cancel",  forState: .Normal);
-//		btn1.setImage(img1, forState: .Normal)
-//		btn1.setImage(img1, forState: .Highlighted)
-		
-		// Star button.
-//		let img2: UIImage? = UIImage(named: "ic_star_white")
-//		let btn2: FlatButton = FlatButton()
-//		btn2.pulseColor = MaterialColor.white
-//		btn2.pulseFill = true
-//		btn2.pulseScale = false
-//		btn2.setImage(img2, forState: .Normal)
-//		btn2.setImage(img2, forState: .Highlighted)
-//		
-//		// Search button.
-//		let img3: UIImage? = UIImage(named: "ic_search_white")
-//		let btn3: FlatButton = FlatButton()
-//		btn3.pulseColor = MaterialColor.white
-//		btn3.pulseFill = true
-//		btn3.pulseScale = false
-//		btn3.setImage(img3, forState: .Normal)
-//		btn3.setImage(img3, forState: .Highlighted)
+//		navigationBarView.titleLabelInsetsRef.left = 64
+
+		let cancelButton: FlatButton = FlatButton()
+		cancelButton.pulseColor = MaterialColor.white
+		cancelButton.pulseFill = true
+		cancelButton.pulseScale = false
+		cancelButton.titleLabel?.font = RobotoFont.boldWithSize(12)
+		cancelButton.setTitle("Cancel",  forState: .Normal);
+		navigationBarView.leftButtonsInsets = MaterialInsets.Square2
+
 		
 		// Add buttons to left side.
-//		navigationBarView.leftButtons = [cancelButton]
+		navigationBarView.leftButtons = [cancelButton]
 		
 		// Add buttons to right side.
 //		navigationBarView.rightButtons = [btn2, btn3]
