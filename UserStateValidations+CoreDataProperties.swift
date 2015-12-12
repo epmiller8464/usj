8fd517fee4f0ccc3bbfc59d23860fe7b3de9afa6
Eric Miller <epmiller8464@gmail.com>
2015-12-12 00:01:33 -0600

    sweat

---
+++ UserStateValidations+CoreDataProperties.swift
@@ -0,0 +1,18 @@
+//
+//  UserStateValidations+CoreDataProperties.swift
+//  usj
+//
+//  Created by ghostmac on 12/12/15.
+//  Copyright © 2015 ghostmac. All rights reserved.
+//
+//  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
+//  to delete and recreate this implementation file for your updated model.
+//
+
+import Foundation
+import CoreData
+
+extension UserStateValidations {
+
+
+}
---
+++ UserStateValidations.swift
@@ -0,0 +1,17 @@
+//
+//  UserStateValidations.swift
+//  usj
+//
+//  Created by ghostmac on 12/12/15.
+//  Copyright © 2015 ghostmac. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class UserStateValidations: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
